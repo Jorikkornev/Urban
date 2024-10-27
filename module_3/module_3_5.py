4686f2b061dfc5392c01acfea46e550b5adfe3a6
Evgeniy Kornev <jorikkornev@gmail.com>
2024-10-27 18:55:41 +0300

    Do module_3_5

---
+++ module_3/module_3_5.py
@@ -0,0 +1,15 @@
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    if len(str_number) <= 1:
+        return int(str_number)  # Если длина строки с числом не больше 1, возвращаем это число
+    else:
+        first = int(str_number[0])  # Получаем первую цифру числа
+        rest_of_number = int(str_number[1:]) # Отрезаем первую цифру от числа
+        result = get_multiplied_digits(rest_of_number) # Вычисляем произведение оставшихся цифр
+        return first * result  # Возвращаем результат умножения первой цифры на произведение остальных цифр
+
+# Пример использования функции
+num = 31124578434
+res = get_multiplied_digits(num)
+print("Произведение цифр числа", num, "равно", res)
