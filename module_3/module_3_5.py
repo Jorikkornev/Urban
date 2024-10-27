063c88f42f48ae2e68e92bea92e981dea1a632c9
Evgeniy Kornev <jorikkornev@gmail.com>
2024-10-27 19:37:03 +0300

    Добавил исключение для 100

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -1,6 +1,8 @@
 def get_multiplied_digits(number):
     """Пример использования рекурсии. Функция возвращает произведение чисел, составляющих число"""
     str_number = str(number)
+    while str_number[-1] == '0':  #  Проверка на наличия нуля последним символом
+        str_number = str_number[:-1]
     if len(str_number) <= 1:
         return int(str_number)  # Если длина строки с числом не больше 1, возвращаем это число
     else:
@@ -10,6 +12,13 @@ def get_multiplied_digits(number):
         return first * result  # Возвращаем результат умножения первой цифры на произведение остальных цифр
 
 # Пример использования функции
-num = 31124578434
-res = get_multiplied_digits(num)
-print("Произведение цифр числа", num, "равно", res)
+num1 = 42000
+num2 = 1001030
+num3 = 2203400
+
+res1 = get_multiplied_digits(num1)
+res2 = get_multiplied_digits(num2)
+res3 = get_multiplied_digits(num3)
+print("Произведение цифр числа", num1, "равно", res1)
+print("Произведение цифр числа", num2, "равно", res2)
+print("Произведение цифр числа", num3, "равно", res3)
