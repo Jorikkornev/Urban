3c262f5659d44f491c4d3f37053f436d169494e2
Evgeniy Kornev <jorikkornev@gmail.com>
2024-11-01 02:59:54 +0300

    Сделал сРазобрался, дело в положении super()

--- module_6/module_6_3.py
+++ module_6/module_6_3.py
@@ -3,7 +3,7 @@ class Horse:
         super().__init__()
         self.x_distance = 0
         self.sound = "Frrr"
-        print(self.sound, super())
+        print(self.sound, 'I am a horse')
 
     def run(self, dx):
         self.x_distance += dx
@@ -13,6 +13,7 @@ class Eagle:
     def __init__(self):
         self.y_distance = 0
         self.sound = 'I train, eat, sleep, and repeat'
+        print(self.sound, "I am an eagle")
 
     def fly(self, dy):
         self.y_distance += dy
@@ -28,14 +29,14 @@ class Pegasus(Horse, Eagle):
         return self.x_distance, self.y_distance
 
     def voice(self):
-        return print(self.sound)
+        print(self.sound)
 
 
 if __name__ == '__main__':
     p1 = Pegasus()
-    test_Horse = Horse()
-    test_Eagle = Eagle()
-    print('Horse: ', test_Horse, 'Eagle: ', test_Eagle.sound, 'Pegasus: ', p1.sound)
+    #test_Horse = Horse()
+    #test_Eagle = Eagle()
+    #print('Horse: ', test_Horse, 'Eagle: ', test_Eagle.sound, 'Pegasus: ', p1.sound)
     print(Pegasus.mro())
     print(p1.get_pos())
     p1.move(10, 15)
