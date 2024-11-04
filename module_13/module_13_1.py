8d4a1bb4172a697b58039e4339552fd6e8c3a206
Evgeniy Kornev <jorikkornev@gmail.com>
2024-11-05 00:20:40 +0300

    ЬщвгИсправил ачения в тесте

--- module_13/module_13_1.py
+++ module_13/module_13_1.py
@@ -20,9 +20,9 @@ async def start_tournament():
         #return tasks
         await asyncio.gather(*tasks)
     await tasks"""
-    task1 = asyncio.create_task(start_strongman('Амбал', 10))
-    task2 = asyncio.create_task(start_strongman('Стрелка', 3))
-    task3 = asyncio.create_task(start_strongman('Барабанщик', 5))
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
     await asyncio.gather(task1, task2, task3)
 
 if __name__ == '__main__':
